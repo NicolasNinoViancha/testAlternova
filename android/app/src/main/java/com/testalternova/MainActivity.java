4becf4a8dc50fd958a01f58e07e27738bcab8b3e
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-07 15:34:54 -0500

    📦 NEW: install react-navigation

--- android/app/src/main/java/com/testalternova/MainActivity.java
+++ android/app/src/main/java/com/testalternova/MainActivity.java
@@ -1,5 +1,8 @@
 package com.testalternova;
 
+//install react-navigation
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -14,6 +17,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "testAlternova";
+  }
+
+  /**
+   * install react-navigation
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
