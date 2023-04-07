49875d98949901e108a22569bef03f75e7c9d44b
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-07 16:27:12 -0500

    📦 NEW: HomeNavigator component

---
+++ src/routes/HomeNavigator/HomeNavigator.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { DetailScreen } from '@src/screens';
+import TapNavigator from './TapNavigator';
+
+const Stack = createNativeStackNavigator();
+
+const HomeNavigator = () => (
+    <Stack.Navigator initialRouteName="MainTap">
+        <Stack.Screen
+            name="Detail"
+            component={DetailScreen} />
+        <Stack.Screen
+            name="MainTap"
+            options={{
+                headerShown: false
+            }}
+            component={TapNavigator} />
+    </Stack.Navigator>
+);
+
+export default HomeNavigator;
