424ad8c3643690bf0e7a81c2dda13c60b8980b4e
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-07 17:26:45 -0500

    📦 NEW: Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+//additional libraries
+import RNIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+//models
+import { PropsIcon } from './models';
+//component
+const Icon = ({
+    name,
+    color,
+    size
+}: PropsIcon) => (
+    <RNIcon
+        name={name}
+        size={size}
+        color={color} />
+);
+
+export default Icon;
