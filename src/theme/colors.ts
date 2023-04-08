f5da0b2f7ca1dc20ec855efe24240d2a7e81333f
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-07 21:56:47 -0500

    📦 NEW: colors object

---
+++ src/theme/colors.ts
@@ -0,0 +1,17 @@
+export const colors = {
+    dark: {
+        background: '#1D1D1B',
+        text: '#FFFFFF'
+    },
+    light: {
+        background: '#FFFFFF',
+        text: '#1D1D1B'
+    },
+    common: {
+        green: '#3AC7A4',
+        pink: '#E61D6D',
+        gray: '#D4D4D4',
+        white: '#FFFFFF',
+        orange: '#FF6B03'
+    }
+};
