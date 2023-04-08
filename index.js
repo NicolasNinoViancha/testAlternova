1be1604da75200aa822d36d3b04219b945c64c55
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-07 21:55:11 -0500

    👌 IMPROVE: implementation ProviderTheme

--- index.js
+++ index.js
@@ -5,5 +5,13 @@
 import { AppRegistry } from 'react-native';
 import App from '@src/App';
 import { name as appName } from './app.json';
+import { ProviderContext as ProviderTheme } from '@src/context/Theme';
 
-AppRegistry.registerComponent(appName, () => App);
+const upTask = () => (
+    <ProviderTheme>
+        <App />
+    </ProviderTheme>
+);
+
+
+AppRegistry.registerComponent(appName, () => upTask);
