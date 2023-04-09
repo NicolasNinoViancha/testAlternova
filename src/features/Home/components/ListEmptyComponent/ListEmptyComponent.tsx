473545a9418a69ac4160699a08df663cab7a1e11
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-08 22:20:04 -0500

    📦 NEW: ListEmptyComponent component

---
+++ src/features/Home/components/ListEmptyComponent/ListEmptyComponent.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Text, ActivityIndicator } from 'react-native';
+//additional components
+import { ButtonText } from '@src/components';
+//models
+import { PropsListEmptyComponent } from './models';
+//styles
+import { styles } from './styles';
+import { COLORS } from '@src/theme';
+//component
+const ListEmptyComponent = ({
+    loading = false,
+    error = false,
+    onRefetch
+}: PropsListEmptyComponent) => {
+    if (loading)
+        return (
+            <View style={styles.container}>
+                <View style={styles.ctnLoading}>
+                    <ActivityIndicator
+                        size="large"
+                        color={COLORS.common.black} />
+                    <Text style={styles.textLoading}>
+                        {`Cargando...`}
+                    </Text>
+                </View>
+            </View>
+        );
+    if (error)
+        return (
+            <View style={styles.container}>
+                <ButtonText
+                    text=""
+                    onPress={onRefetch} />
+            </View>
+        );
+    return (
+        <View style={styles.container}>
+            <Text style={[styles.textLoading, { textAlign: 'center' }]}>
+                {`No existen elementos`}
+            </Text>
+        </View>
+    );
+}
+export default ListEmptyComponent;
---
+++ src/features/Home/components/ListEmptyComponent/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet, Dimensions } from "react-native";
+import { COLORS } from '@src/theme';
+
+const { height, width } = Dimensions.get('window');
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: height * 0.9,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    ctnLoading: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textLoading: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: COLORS.common.black,
+        marginLeft: width * 0.02
+    }
+})
