6c61e4eacba2a1fca669d22eb84a8a36091f0795
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-08 22:22:14 -0500

    👌 IMPROVE: add HomeScreen component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
-import { Icon } from '@src/components';
-const LogoCompany = require('@src/assets/Images/logoCompany.png');
+import Home from '@src/features/Home/container';
+import { PropsScreen } from '@src/features/Home/models';
 
-const HomeScreen = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
-            <View style={{ width: '80%', height: 100 }}>
-                <Image
-                    style={{ width: '100%', height: '100%' }}
-                    resizeMode={'contain'}
-                    source={LogoCompany} />
-            </View>
-        </View>
-    );
-}
+const HomeScreen = (props: PropsScreen) => (<Home {...props} />);
 
 export default HomeScreen;
