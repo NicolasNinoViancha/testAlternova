7fcedaf224f177c71084c9ca70f7c0bd777d75e1
NicolasNinoViancha <nicolasemilioninoviancha@gmail.com>
2023-04-08 20:39:53 -0500

    👌 IMPROVE: implementation subscriber for auth state changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 //additional libraries
 import { NavigationContainer } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 //additional components
 import { ViewBackground } from '@src/components';
+import { SplashScreen, LoginScreen } from '@src/screens';
 //routes
 import { LoginNavigator, HomeNavigator } from '@src/routes';
-import { View } from 'react-native';
-
-const isLogin = false;
 //component
-const App = () => (
-  <NavigationContainer>
-    <ViewBackground>
-      {
-        isLogin
-          ? <HomeNavigator />
-          : <LoginNavigator />
-      }
-    </ViewBackground>
-  </NavigationContainer>
-);
+const App = () => {
+  //states
+  const [initializing, setInitializing] = useState<boolean>(true);
+  const [isLogin, setIsLogin] = useState<boolean>();
+  const [user, setUser] = useState<any>();
+  //functions
+  // Handle user state changes
+  function onAuthStateChanged(user: any) {
+    setTimeout(() => {
+      setUser(user);
+      setIsLogin(user !== null);
+      if (initializing)
+        setInitializing(false);
+      console.log({ user });
+    }, 1000);
+  }
+  //effects
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+  //main component
+  if (initializing)
+    return (
+      <ViewBackground>
+        <SplashScreen />
+      </ViewBackground>
+    );
+  return (
+    <NavigationContainer>
+      <ViewBackground>
+        {
+          isLogin
+            ? <HomeNavigator />
+            : <LoginNavigator />
+        }
+      </ViewBackground>
+    </NavigationContainer>
+  )
+};
 
 export default App;
